0bd8369901558bc3e3f746a8096de54600078011
ramkrsna <rreddy@redhat.com>
2018-11-20 20:08:10 +0530

    prometheus templates for glusterfs

--- openshift_cic/cic.py
+++ openshift_cic/cic.py
@@ -44,6 +44,7 @@ def perform_operation():
     print ("3. Storage for Applications + Metrics ")
     print ("4. Storage for Applications + Registry + Logging + Metrics")
     print ("5. Storage for Applications Only")
+    print ("6. Storage for Applications + Prometheus Cluster Metrics")
     print (60 * '-')
 
     is_valid=0
@@ -525,6 +526,137 @@ def perform_operation():
                                              raw_devices=json.dumps(raw_devices),
                                              raw_storage_size=raw_storage_size)
                     #Print the output
+                    print(output)
+
+    elif choice == 6:
+            print (60 * '-')
+            print ("For this configuration 7 nodes are recommended")
+            print ("With a minimum of 3 required ")
+            print (60 * '-' )
+            avail_hosts = int(raw_input("How many nodes are available ?:  "))
+
+            if avail_hosts >= 6:
+                    app_hosts =  raw_input("What hosts will be used for application storage (IP/FQDN) ?: ")
+                    app_hosts = utils.check_input(app_hosts)
+                    utils.min_hosts(app_hosts)
+                    utils.host_not_valid(app_hosts)
+                    raw_devices = raw_input("What are the raw storage devices for these hosts(/dev/<device>) ?: ")
+                    raw_devices = utils.check_input(raw_devices)
+                    raw_storage_size = int(raw_input("What is the size of each raw storage device (GB) ?: "))
+                    registry_pvsize = int(raw_input("What is the size for the registry persistent volume (GB)?: "))
+                    replica_metrics = 1
+                    metrics_pvsize = int(raw_input("What is the size for each metrics persistent volume (GB) ?: "))
+                    met_hosts =  raw_input("What hosts will be used for CNS metrics backend storage  (IP/FQDN) ?:  ")
+                    met_hosts = utils.check_input(met_hosts)
+                    utils.met_in_use(met_hosts, app_hosts)
+                    met_devices = raw_input("What are the raw storage devices for metrics backend on these hosts (/dev/<device>) ?: ")
+                    met_devices = utils.check_input(met_devices)
+                    met_storage_size = int(raw_input("What is the size of each raw storage device (GB) ? : "))
+                    zone = [1,2,3]
+
+                    min_block_host_vol_size =  ( metrics_pvsize * replica_metrics)
+                    block_host_size = int ( min_block_host_vol_size + (30/100.0) * min_block_host_vol_size)
+
+                    # Two cluster total storage calculation
+                    cluster_storage = len(raw_devices) * raw_storage_size * len(app_hosts)
+                    # App storage in Replica 3
+                    total_avail_store = cluster_storage / 3.0
+                     # Reg store Replica-3
+                    total_reg_store = (met_storage_size * len(met_devices) * len(met_hosts)) / 3.0
+
+                    block_calc = registry_pvsize + block_host_size
+                    totalalloc = block_calc
+
+                    print "# Cluster 1"
+                    print "# Total Storage allocated (GB) = 0"
+                    print "# Total Storage available (GB) = %d" % total_avail_store
+                    print "   "
+                    print "# Cluster 2"
+                    print "# Total Storage allocated (GB) = %d" % totalalloc
+                    print "# Total Storage available (GB) = %d" % total_reg_store
+
+                    if  registry_pvsize > met_storage_size and totalalloc < total_reg_store :
+                            print "\033[91mWarning one or more persistent volumes are"
+                            print "larger than the raw storage device size\033[0m"
+                            print "  "
+                            exit()
+
+                    elif registry_pvsize < met_storage_size and totalalloc > total_reg_store :
+                            print "\033[91mWarning your Total Storage available is less "
+                            print "than the Total Storage allocated\033[0m"
+                            exit()
+
+                    elif registry_pvsize > met_storage_size and totalalloc > total_reg_store :
+                            print "\033[91mWarning one or more persistent volumes are"
+                            print "larger than the raw storage device size"
+                            print "  "
+                            print "Warning your Total Storage available is less "
+                            print "than the Total Storage allocated\033[0m"
+                            exit()
+                    print " "
+                    template = utils.get_template_input(ocpver, 'appmet-multi.j2')
+                    output = template.render(ver=ver,app_hosts=app_hosts,
+                                             raw_devices=json.dumps(raw_devices),
+                                             raw_storage_size=raw_storage_size,
+                                             block_host_size=block_host_size,
+                                             registry_pvsize=registry_pvsize,
+                                             metrics_pvsize=metrics_pvsize,
+                                             met_hosts=met_hosts,
+                                             met_devices=met_devices,
+                                             met_storage_size=met_storage_size)
+                    print(output)
+
+            else:
+                    app_hosts =  raw_input("What hosts will be used for application storage (IP/FQDN) ?: ")
+                    app_hosts = utils.check_input(app_hosts)
+                    utils.min_hosts(app_hosts)
+                    utils.host_not_valid(app_hosts)
+                    raw_devices = raw_input("What are the raw storage devices for these hosts(/dev/<device>) ?: ")
+                    raw_devices = utils.check_input(raw_devices)
+                    raw_storage_size = int(raw_input("What is the size of each raw storage device (GB) ?: "))
+                    registry_pvsize = int(raw_input("What is the size for the registry persistent volume (GB)?: "))
+                    replica_metrics = 1
+                    metrics_pvsize = int(raw_input("What is the size for each metrics persistent volume (GB) ?: "))
+                    zone = [1,2,3]
+
+                    min_block_host_vol_size =  ( metrics_pvsize * replica_metrics)
+                    block_host_size = int ( min_block_host_vol_size + (30/100.0) * min_block_host_vol_size)
+                    # Single cluster total storage calculation
+                    cluster_storage = len(raw_devices) * raw_storage_size * len(app_hosts)
+                    total_avail_store = cluster_storage / 3.0
+                    block_calc = registry_pvsize + block_host_size
+                    totalcalc = block_calc
+
+                    print "# Cluster 1"
+                    print "# Total Storage allocated (GB) = %d" % block_calc
+                    print "# Total Storage available (GB) = %d" % total_avail_store
+
+                    if block_calc > total_avail_store and registry_pvsize < total_avail_store:
+                            print "\033[91mWarning Your Total Storage available is less "
+                            print "than the Total Storage allocated\033[0m"
+                            exit()
+
+                    elif block_calc < total_avail_store and registry_pvsize > total_avail_store:
+                            print "\033[91m Warning one or more persistent volumes are"
+                            print "larger than the raw storage device size\033[0m"
+                            print "  "
+                            exit()
+
+                    elif block_calc > total_avail_store and registry_pvsize > total_avail_store:
+                            print "\033[91mWarning one or more persistent volumes are"
+                            print "larger than the raw storage device size"
+                            print "  "
+                            print "Warning your Total Storage available is less "
+                            print "than the Total Storage allocated\033[0m"
+                            exit()
+                    print " "
+                    template = utils.get_template_input(ocpver, 'appmet.j2')
+                    output = template.render(ver=ver,app_hosts=app_hosts,
+                                             raw_devices=json.dumps(raw_devices),
+                                             raw_storage_size=raw_storage_size,
+                                             block_host_size=block_host_size,
+                                             registry_pvsize=registry_pvsize,
+                                             metrics_pvsize = metrics_pvsize)
                     print(output)
 
     else:
